f23193c4746e244189ce4f54a67bc4e0b54bb202
hauthithanhhuyen <162555521+hauthithanhhuyen@users.noreply.github.com>
2025-03-28 11:02:52 +0700

    Add files via upload

---
+++ bai_tap_3_data.sql
@@ -0,0 +1,21 @@
+﻿SELECT 
+    SV.Masv, 
+    SV.HoTen, 
+    LHP.MaLopHP, 
+    LHP.TenLopHP, 
+    MH.Tenmon, 
+    SUM(CAST(D.diem AS FLOAT)) * (1 - CAST(DKMH.PhanTramThi AS FLOAT) /100) AS DiemThanhPhan
+FROM 
+    SinhVien1 SV
+JOIN 
+    DKMH ON SV.Masv = DKMH.Masv
+JOIN 
+    LopHP LHP ON DKMH.MaLopHP = LHP.MaLopHP
+JOIN 
+    MonHoc MH ON LHP.MaMon = MH.MaMon
+JOIN 
+    Diem D ON DKMH.id_dk = D.id_dk
+WHERE 
+    SV.Masv = 'k111' -- Thay MASV123 bằng mã sinh viên cần tìm
+GROUP BY 
+    SV.Masv, SV.HoTen, LHP.MaLopHP, LHP.TenLopHP, MH.Tenmon, DKMH.PhanTramThi;
